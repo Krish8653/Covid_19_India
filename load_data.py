5a51715a24d3f7d9d12fbb59cfe3f9c9c5af9fd4
Krish8653 <krishnendrabhattacharyya@yahoo.com.au>
2020-05-30 00:14:50 +0530

    adding latest file

--- load_data.py
+++ load_data.py
@@ -2,6 +2,7 @@ import pandas as pd
 import requests
 import datetime
 import pyodbc
+from openpyxl import load_workbook
 # from bs4 import BeautifulSoup
 # from tabulate import tabulate
 
@@ -26,11 +27,25 @@ query = "INSERT INTO [dbo].[MOHFW] ([State],[Total_Confirmed],[Total_Cured],[Tot
 
 for index,rows in df.iterrows():
     values = (rows[0],rows[1],rows[2],rows[3],rows[4])
-    print(values)
+    #print(values)
     cursor.execute(query, values)
     sql_conn.commit()
+
+'''UPDATING THE Corona_database_extract.xlsx file'''
+select_query = "Select [State],[Total_Confirmed],[Total_Cured],[Total_Death],FORMAT([Date],'dd-MM-yyyy') AS DATE,[Confirmed_cases_on_this_day],[Recovered_cases_on_this_day],[Death_cases_on_this_day] from [dbo].[MOHFW]  where [Date] = CAST(Getdate() AS DATE);"
+select_df = pd.read_sql(select_query, sql_conn)
+book = load_workbook("D:\\Projects\\Covid_19_India\\Corona_database_extract.xlsx")
+writer = pd.ExcelWriter('D:\\Projects\\Covid_19_India\\Corona_database_extract.xlsx', engine= 'openpyxl') # pylint: disable=abstract-class-instantiated
+writer.book = book
+writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
+
+reader = pd.read_excel(open("D:\\Projects\\Covid_19_India\\Corona_database_extract.xlsx",'rb'),sheet_name= 'Latest Data')
+select_df.to_excel(writer,sheet_name= 'Latest Data' ,index=False,header=False,startrow=len(reader)+1)
+
+writer.save()
+writer.close()
+
 cursor.close()
 sql_conn.close()
 
-# print(df.shape)
 
